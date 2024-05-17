94e8bb6dedf3e424185dd22a6889489193f3a8f2
KoplanyiDavid <koplanyidavid@edu.bme.hu>
2024-05-17 11:45:43 +0200

    simplification

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 is_on = True
-cm = CoffeeMaker()
-mm = MoneyMachine()
-m = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
 
 if __name__ == "__main__":
     while is_on:
-        user_input = input(f"Choose coffee ({m.get_items()}): ")
+        user_input = input(f"Choose coffee ({menu.get_items()}): ")
         if user_input == "espresso" or user_input == "cappuccino" or user_input == "latte":
-            drink = m.find_drink(user_input)
-            if cm.is_resource_sufficient(drink):
-                if mm.make_payment(drink.cost):
-                    cm.make_coffee(drink)
+            drink = menu.find_drink(user_input)
+            if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
         elif user_input == "report":
-            cm.report()
+            coffee_maker.report()
+        elif user_input == "off":
+            is_on = False
         else:
             print("Wrong input!")
